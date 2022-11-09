0b8c1d5dc5721684efbe0cd04ec3d1839b4869a8
Eldar Skhatum <79996321123@ya.ru>
2022-11-09 23:57:46 +0400

    added Example 005 condition

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Woohoo! It's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
